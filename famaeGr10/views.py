220bf9207187927fd1c30d1985b4020cea6ced01
ByBell <gaet97@live.fr>
2018-12-18 18:17:49 +0100

    3 routes - api/all/ - api/city/(?P<id>[\s\w]+)$‘ - api/search/(?P<search>[\s\w]+)$

--- famaeGr10/views.py
+++ famaeGr10/views.py
@@ -6,20 +6,34 @@ import pandas as pd
 contaminants = pd.read_csv('famaeGr10/data/contaminants2.csv', index_col=0)
 zipcodes = pd.read_csv('famaeGr10/data/zip_codes2.csv')
 
-data = contaminants.merge(zipcodes, left_on='locations_served', right_on='county')
-data = data[['city', 'lat', 'long']]
+df = contaminants.merge(zipcodes, left_on='locations_served', right_on='county')
+df_simplified = df[['city', 'lat', 'long']]
 
 
 def map(request):
-    return render(request, 'map.html')
+  return render(request, 'map.html')
 
-def jsonByZipcode(request, zipcode):
+def byCity(request, id=None):
+  print("id :", id)
 
-    data2 = data.drop_duplicates().reset_index()
-    del data2['index']
+  data_no_duplicate = df_simplified.drop_duplicates().reset_index()
+  data_full = df.drop_duplicates().reset_index()
 
-    data2 = data2.head()
-    
-    jsonData = data2.to_json(orient="records")
+  if id == None:
+    final_data = data_no_duplicate
+  else:
+    try:
+      final_data = data_full[data_full['index'] == int(id)]
+    except:
+      return HttpResponse({"Error : Bad 'id' type (not int)"}, content_type = 'application/json')
 
-    return HttpResponse(jsonData, content_type = 'application/json')
+  return HttpResponse(final_data.to_json(orient="records"), content_type = 'application/json')
+
+def autocomplete(request, search=None):
+  print("search :", search)
+
+  custom_data = df_simplified.drop_duplicates().reset_index()
+
+  final_data = custom_data[custom_data['city'].str.contains(search.upper())]
+
+  return HttpResponse(final_data.to_json(orient="records"), content_type = 'application/json')
